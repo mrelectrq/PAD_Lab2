de3d18dc57a7014ad139bf1b5940708928b20622
cristian.ionas <cris.ionas@gmail.com>
2020-10-20 20:11:17 +0300

    Implementing Consistent Hashing

---
+++ Proxy/Proxy/Proxy/Controllers/TestController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Proxy.Models;
+
+namespace Proxy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public void Get()
+        {
+            List<Server> servers = new List<Server>();
+            for (int i = 0; i < 10; i++)
+            {
+                servers.Add(new Server(i));
+            }
+            ConsistentHash<Server> ch = new ConsistentHash<Server>();
+            ch.Init(servers);
+
+            int search = 10;
+
+            SortedList<int, int> ay1 = new SortedList<int, int>();
+            for (int i = 0; i < search; i++)
+            {
+                int temp = ch.GetNode(i.ToString()).ID;
+
+                ay1[i] = temp;
+            }
+
+            ch.Remove(servers[1]);
+        }
+    }
+}
---
+++ Proxy/Proxy/ProxyTests/Controllers/TestControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Proxy.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proxy.Controllers.Tests
+{
+    [TestClass()]
+    public class TestControllerTests
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+            try
+            {
+                var controller = new TestController();
+                controller.Get();
+            }
+            catch { }
+        }
+    }
+}
+
