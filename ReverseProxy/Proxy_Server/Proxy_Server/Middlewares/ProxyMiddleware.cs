b64c51520518f608adb1aa3acc0ed34f64a734b0
mrelectrq <tiora100bass@gmail.com>
2020-10-22 22:58:12 +0300

    Added ProxyMiddleware, Injected ServiceStoreService.

---
+++ ReverseProxy/Proxy_Server/Proxy_Server/Middlewares/ProxyMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Proxy_Server.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Proxy_Server.Middlewares
+{
+    public class ProxyMiddleware
+    {
+        private static HttpClient _httpClient = new HttpClient();
+        private readonly RequestDelegate _nextMiddleware;
+        private readonly IServerStorage _storage;
+        public ProxyMiddleware(RequestDelegate next, IServiceProvider serviceScope)
+        {
+            _storage = serviceScope.GetRequiredService<IServerStorage>();
+            _nextMiddleware = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            
+           var request_destination = BuildUri(context.Request);
+
+            await _nextMiddleware(context);
+        }
+
+
+        private Uri BuildUri(HttpRequest request)
+        {
+            var uri = request.Path;
+
+
+        }
+    }
+}
