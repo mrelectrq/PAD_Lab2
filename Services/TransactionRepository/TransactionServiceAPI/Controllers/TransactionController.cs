7131e3f7318ced472b9696f64ad61e1ec80e3bfe
cristian.ionas <cris.ionas@gmail.com>
2020-12-03 10:10:36 +0200

    Docker implementation

---
+++ Services/TransactionRepository/TransactionServiceAPI/Controllers/TransactionController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer;
+using BusinessLayer.Interfaces;
+using BusinessLayer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TransactionServiceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        ITransaction transaction;
+        public TransactionController()
+        {
+            var _bl = new BusinessManager();
+            transaction = _bl.Transaction();
+        }
+        [HttpPost]
+        public ActionResult<TransactionResponse> Post([FromBody] TransactionMessage message)
+        {
+            var response = transaction.GetTransaction(message);
+            return response.Result;
+        }
+    }
+}
